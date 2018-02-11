030b9f516f4ce2b27b4ecd5e065426ce3dc90d70
dmahw <28464255+dmahw@users.noreply.github.com>
2018-02-11 15:37:18 -0800

    More progress

---
+++ README.txt
@@ -0,0 +1,11 @@
+David Mah
+
+Requirements
+*****************************
+1. Must be using Python 3
+2. Package setuptools must be installed for python 3 
+    "sudo apt-get install python3-setuptools"
+3. Package dpkt must be installed for python 3
+    "sudo pip3 install dpkt"
+4. Package socket must be installed for python 3
+4. And any other dependencies required for setuptools and dpkt
--- TCPTrafficAnalysis.py
+++ TCPTrafficAnalysis.py
@@ -1,14 +1,28 @@
-import pcapy, sys
+import pcapy, sys, dpkt, socket
+from dpkt.compat import compat_ord
 
 VERBOSE = 1
 
+# Printing mac_addresses
+# http://dpkt.readthedocs.io/en/latest/_modules/examples/print_packets.html?highlight=print%20ip
+def mac_addr(address):
+    return ":".join("%02x" % compat_ord(b) for b in address)
+
+def inet_to_str(inet):
+    return socket.inet_ntop(socket.AF_INET, inet)
+
 def main():
-    if VERBOSE:
-        print("********** Hello, Starting Traffic Analysis")
+    traceFileName = sys.argv[1]
 
-    traceFileName = sys.argv[2]
-    
-    if VERBOSE:
-        print("********** Input form user " + traceFileName)
-    # Main code
+    traceFile = open(traceFileName, "rb")
+    tracePcap = dpkt.pcap.Reader(traceFile)
 
+    for timeStamp, buf in tracePcap:
+        eth = dpkt.ethernet.Ethernet(buf)
+        head = eth.data
+        print(mac_addr(eth.src))
+        print(inet_to_str(head.src))
+        print(head.sport)
+        break
+
+main()
